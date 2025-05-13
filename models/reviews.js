9204db0176c5b6df4a9e7712bbd05726f993b3e5
Deependra kumar <kushdep017@gmail.com>
2025-05-13 16:41:52 +0530

    Review CR*D Operation

--- models/reviews.js
+++ models/reviews.js
@@ -1,10 +1,9 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const reviewShema = new Schema({
-    body:String,
-    rating:Number``
-})
+  body: String,
+  rating: Number,
+});
 
-
-module.exports = mongoose.model('Review',reviewShema)
+module.exports = mongoose.model("Review", reviewShema);
--- schemas.js
+++ schemas.js
@@ -1,19 +1,18 @@
-const Joi = require('joi')
+const Joi = require("joi");
 
 module.exports.rentLocationSchema = Joi.object({
-    rentloc:Joi.object({
-        title:Joi.string().required(),
-        location:Joi.string().required(),
-        price:Joi.number().required(),
-        image:Joi.string().required(),
-        description:Joi.string().required()
-    }).required()
+  rentloc: Joi.object({
+    title: Joi.string().required(),
+    location: Joi.string().required(),
+    price: Joi.number().required(),
+    image: Joi.string().required(),
+    description: Joi.string().required(),
+  }).required(),
 });
 
-
-
-
-
-
-
-
+module.exports.reviewSchema = Joi.object({
+  review: Joi.object({
+    rating: Joi.number().required().min(1).max(5),
+    body: Joi.string().required(),
+  }).required(),
+});
